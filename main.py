e5b82b53ab3acc97ecbd446d50431d2c5bf976c4
Philip Cullen <philip.cullen93@gmail.com>
2026-07-29 17:03:46 +0100

    Created driver model and connected it to Jolpica API

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from models.driver import Driver
+
+def main():
+
+    driver = Driver(
+        driver_id="max_verstappen",
+        first_name="Max",
+        last_name="Verstappen",
+        abbreviation="VER",
+        number=1,
+        nationality="Dutch",
+        date_of_birth="1997-09-30",
+    )
+
+    print(driver)
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/driver.py
@@ -0,0 +1,31 @@
+class Driver:
+    def __init__(
+            self,
+            driver_id,
+            first_name,
+            last_name,
+            abbreviation,
+            number,
+            nationality,
+            date_of_birth,
+    ):
+        self.driver_id = driver_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.abbreviation = abbreviation
+        self.number = number
+        self.nationality = nationality
+        self.date_of_birth = date_of_birth
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return (
+            f"{self.number} | "
+            f"{self.abbreviation} | "
+            f"{self.full_name}"
+        )
+
+
