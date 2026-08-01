8001f24f70db0a35502e638e5c76b8262eef9905
Philip Cullen <philip.cullen93@gmail.com>
2026-08-01 20:31:59 +0100

    Created a make_request function to tidy up jolpica.py and remove redundant code

--- api/jolpica.py
+++ api/jolpica.py
@@ -2,24 +2,24 @@ import requests
 
 BASE_URL = "https://api.jolpi.ca/ergast/f1"
 
+def make_request(endpoint):
+    url = f"{BASE_URL}/{endpoint}"
+    response = requests.get(url)
+    response.raise_for_status
+    return response.json()
+
 def get_current_drivers():
     # Retrieves the current list of drivers from Jolpica API
-    url = f"{BASE_URL}/current/drivers.json"
-
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
+    return make_request(
+        "current/drivers.json"
+    )
 
 def get_current_constructors():
-    url = f"{BASE_URL}/current/constructors.json"
-
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
+    return make_request(
+        "current/constructors.json"
+    )
 
 def get_current_circuits():
-    url = f"{BASE_URL}/current/circuits.json"
-
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
+    return make_request(
+        "current/circuits.json"
+    )
